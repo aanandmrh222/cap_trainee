565eb52ebc209fa3811cdc96d01f2e3501e70014
Aanand Kumar <aanandmrh222@gmail.com>
2026-01-04 22:03:57 +0530

    day11-string

---
+++ C#/Day11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    public static void Main()
+    {
+        /*
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Hello");
+        sb.Append(" ");
+        sb.Append("World");
+        Console.WriteLine(sb.ToString());
+
+        sb.Append(" Text");
+        sb.AppendLine(" Line");
+
+        sb.Insert(0, "Hii ");
+        sb.Remove(0, 5);
+        sb.Replace("Line", "Llllll");
+        Console.WriteLine(sb.ToString());
+        sb.Clear();
+        Console.WriteLine(sb.ToString());
+
+
+        StringBuilder sb = new StringBuilder();
+        Console.WriteLine(GC.GetTotalMemory(true));
+        for(int i=0;i<1000; i++)
+        {
+            sb.Append(i);
+        }
+        string res = sb.ToString();
+        Console.WriteLine(GC.GetTotalMemory(true));
+
+
+
+        StringBuilder sb1 = new StringBuilder("Hoo");
+        StringBuilder sb2 = new StringBuilder("Hoo");
+        Console.WriteLine(sb2==sb1);   // == checks reference equality (memory address)
+        Console.WriteLine(sb1.Equals(sb2));  // Equals() is NOT overridden in StringBuilder , It also checks reference equality, not content
+        StringBuilder sb3 = sb2;  // sb3 points to the SAME object as sb2
+        Console.WriteLine(sb3.Equals(sb2));
+        Console.WriteLine(sb3.ReferenceEquals(sb1,sb2)); 
+
+
+        */
+
+        
+
+
+
+    }
+}
